bca7205ba230f1e06c16bf7a138bc2e341e403ac
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-26 07:35:31 -0400

    Basic home page layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 import { fetchPosts, square } from './lib'
 
-document.getElementById('message').innerText =
-  'Hello World! The Square of 2 is " + square(2)'
-
 fetchPosts()
+
+const toggle = document.getElementById('toggle')
+
+// Toggle navigation
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+)
+
+// hide modal on outside click
+window.addEventListener('click', (e) =>
+  e.target === modal ? modal.classList.remove('show-modal') : false
+)
